975ef4f546aad8bdcafe7f94f82bc649ccfb7e51
Vagner José Santos de Mello <vagnerjsmello@gmail.com>
2020-03-15 12:18:00 +0000

    Add ShoppingCartComponent and events

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ShoppingCartService } from "./shopping-cart.service";
 @Component({
   selector: 'mt-shopping-cart',
   templateUrl: './shopping-cart.component.html'
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
+  }
+
+  items():any[]{
+    return this.shoppingCartService.items
+  }
+
+  removeItem(item : any){
+    return this.shoppingCartService.remove(item)
+  }
+
+  addItem(item: any){
+    return this.shoppingCartService.add(item)
+  }
+  clear(){
+    return this.shoppingCartService.clear()
+  }
+  total(): number{
+    return this.shoppingCartService.total()
   }
 
 }
