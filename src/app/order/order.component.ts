ad09b72bb905c1db853bf919ca95fe192bf058da
Vagner José Santos de Mello <vagnerjsmello@gmail.com>
2020-04-04 14:27:02 +0100

    RadioComponent - ControlValueAccessor NG_VALUE_ACCESSOR

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON' },
+    {label: 'Cartão de Débito', value: 'DEB' },
+    {label: 'Cartão Refeição', value: 'REF' }
+  ]
   constructor() { }
 
   ngOnInit() {
