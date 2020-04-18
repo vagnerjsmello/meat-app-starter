2f82eeedffbbffc1087857d6cb7da3ea4c15a778
Vagner <vagnerjsmello@gmail.com>
2020-04-18 15:21:46 +0100

    Angular - Shared Modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,33 @@
+import { NgModule, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { InputComponent } from "./input/input.component";
+import { RadioComponent } from "./radio/radio.component";
+import { RatingComponent } from "./rating/rating.component";
+
+
+
+
+@NgModule({
+    declarations: [
+        InputComponent, 
+        RadioComponent,
+        RatingComponent
+    ],
+    imports:[
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
+    exports:[
+        InputComponent, 
+        RadioComponent,
+        RatingComponent,
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule
+    ]
+})
+
+export class SharedModule { }
